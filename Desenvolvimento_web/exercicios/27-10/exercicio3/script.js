db306225a8fb325eeed3b879a0f1e547cf8af1ac
Arcanjo <rafaelarcanjo129@hotmail.com>
2021-11-10 21:13:53 -0300

    Aula do dia 10-11

--- Desenvolvimento_web/exercicios/27-10/exercicio3/script.js
+++ Desenvolvimento_web/exercicios/27-10/exercicio3/script.js
@@ -2,26 +2,23 @@
 window.onload = function(){
     let bnt = document.getElementById("botao")
     bnt.onclick = function(){
-        calcImposto();
+        maiorNum();
     }
 }
 
 
 
-function calcImposto(){
-    var salario = parseFloat(document.getElementById("salario").value)
-    if(salario <1434 ){
-        document.getElementById("imposto").value = "Isento de impostos " 
-    }else if(salario < 2150){
-        document.getElementById("imposto").value = "7,5% de aliquota" + "R$ " + (salario * (7.5/100))
-    }else if(salario < 2866){
-        document.getElementById("imposto").value = "15% de aliquota" + "R$ " + (salario * (15/100))
-    }else if(salario < 3582){
-        document.getElementById("imposto").value = "22,5% de aliquota" + "R$ " + (salario * (22.5/100))
-    }else{
-        document.getElementById("imposto").value = "27,5% de aliquota" + "R$ " + (salario * (27.5/100))
-}}
+function maiorNum(){
+    var num1 = parseInt(document.getElementById("valor1").value)
+    var num2 = parseInt(document.getElementById("valor2").value)
+    if(num1 > num2){
+        document.getElementById("mensagem").value = "O primeiro valor é maior"
+    }else if(num1 < num2){
+        document.getElementById("mensagem").value = "O segundo valor é maior"
+    }else if(num1 = num2){
+        document.getElementById("mensagem").value = "Tem o mesmo valor"
+    }
 
-
+}
 
 
